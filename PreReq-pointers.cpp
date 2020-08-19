0edb915091f06af070e5acecccbc51835d6f42b2
chavangorakh1999 <chavangorakh19999@gmail.com>
2020-08-19 19:23:00 +0530

    syntax and their use

--- PreReq-pointers.cpp
+++ PreReq-pointers.cpp
@@ -3,7 +3,14 @@ using namespace std;
 
 int main()
 {
-    
+    int a=15,*b;
+    cout<<a<<endl;
+    b=&a;
+    //this  $  is used to disclose the adress of the variable stored.
+
+    cout<<*b<<endl;
+
+    //This * is used to reterviews value stored at that particular address.
     return 0;
 }
 
