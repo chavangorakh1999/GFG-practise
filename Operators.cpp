e3951f5dea8613cc1a63f1d694a80e82c5b7375c
chavangorakh1999 <chavangorakh19999@gmail.com>
2020-08-21 16:37:16 +0530

    Associativity of same operator

---
+++ Operators.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+ using namespace std;
+
+ int main()
+ {
+     int x=10,y=20, _=5;
+     cout<<x+y<<endl  ;
+     cout<<x*y<<endl;
+     cout<<x/y<<endl;
+     cout<<"Modulo="<<x%y<<endl;
+
+     cout<<"Associativity="<<y*x/x<<endl;
+
+     cout<<_<<endl;
+
+     return 0;
+ }
