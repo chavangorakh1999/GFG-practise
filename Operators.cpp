510d5cd40e0c6595fa983657087226ffedd06abe
chavangorakh1999 <chavangorakh19999@gmail.com>
2020-08-21 17:31:23 +0530

    problems on operators

--- Operators.cpp
+++ Operators.cpp
@@ -3,15 +3,39 @@
 
  int main()
  {
-     int x=10,y=20, _=5;
-     cout<<x+y<<endl  ;
-     cout<<x*y<<endl;
-     cout<<x/y<<endl;
-     cout<<"Modulo="<<x%y<<endl;
+    //  int x=10,y=20, _=5;
+    //  cout<<x+y<<endl  ;
+    //  cout<<x*y<<endl;
+    //  cout<<x/y<<endl;
+    //  cout<<"Modulo="<<x%y<<endl;
 
-     cout<<"Associativity="<<y*x/x<<endl;
+    //  cout<<"Associativity="<<y*x/x<<endl;
 
-     cout<<_<<endl;
+    //  cout<<_<<endl;
+    
+    //Problem 1
 
-     return 0;
- }
+    // int Variable_1=5;
+    // bool result=(Variable_1==4)&&(Variable_1++);
+    // cout<<result<<endl;
+
+    //Problem 2
+
+    // int Variable_1=5;
+    // bool result=(Variable_1==4)||(Variable_1++);
+    // cout<<result<<endl;
+
+    //Problem 3
+
+    // int Variable_1=5;
+    // bool result=(Variable_1==5)||(Variable_1++);
+    // cout<<result<<endl;
+
+    //Comma Operator (Lowest precedence in the operator table)
+
+    // int x = 1, y = 2, z = 3, a;
+    // a = x, y, z;
+    // cout << a;
+    
+     return 0; 
+ } 
