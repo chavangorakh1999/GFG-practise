574f15a01f677a5a36bb47aeacf345cda0978129
chavangorakh1999 <chavangorakh19999@gmail.com>
2020-08-23 16:59:20 +0530

    playing around with functions

--- function.cpp
+++ function.cpp
@@ -1,20 +1,38 @@
 #include<iostream>
 using namespace std;
-void fun(int x)
-{
-    if(x==0)
-    return;
-    else
-    {
-        cout<<"Geeks"<< " ";
-        fun(x-1);
-        cout<<"For Geeks"<<" "<<x<<" ";
-    }
+// void fun(int x)
+// {
+//     if(x==0)
+//     return;
+//     else
+//     {
+//         cout<<"Geeks"<< " ";
+//         fun(x-1);
+//         cout<<"For Geeks"<<" "<<x<<" ";
+//     }
     
-    return;
-}
+//     return;
+// }
+// int summ()
+// {   int sum=0;
+//     return sum;
+// }
 int main()
 {
-    fun(3);
+    // fun(3);
+    int n;
+
+    cin>>n;
+    static int sum=0;
+    for(int i=1;i<=n;i++)
+    {   
+       
+        if((i%3==0) && (i%5==0))
+        {
+            sum+=i;
+        }
+       cout<<sum<<endl;
+    }
+    
     return 0;
 }
